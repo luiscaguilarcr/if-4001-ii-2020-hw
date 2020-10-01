7da16f1f44389141132db3c5c5912a9d84910f85
Luis Carlos Aguilar Morales <luiscarlos.aguilar@ucr.ac.cr>
2020-10-01 17:09:32 -0600

    Update

--- Tarea_1/Prueba1/Prueba.c
+++ Tarea_1/Prueba1/Prueba.c
@@ -3,18 +3,18 @@
 #include <string.h>
 
 void main(int argc, char *argv[]) {
-    char interactive[] = "strace -c ls > ";
-    char automatic[] = "strace -c ./";
-    char list[] = "/Lista de directorios";
+    char automatic[] = "strace -c ls > "; //Modo automatico 
+    char interactive[] = "strace -c ls "; //Modo interactivo
+    char list[] = "/null";
     
-    if(strcmp(argv[1], "1") == 0){
-        strncat(interactive, &argv[2], sizeof argv[2]);
-        strncat(interactive, list, sizeof list);
-        system(interactive);
-
-    } else if (strcmp(argv[1], "2") == 0) {
-        strncat (automatic, &argv[2], sizeof argv[2]);
+    if(strcmp(argv[1], "1") == 0){ //El uno significa Modo 1 y es automatico
+        strncat(automatic, &argv[2], sizeof argv[2]); //Concatena el comando con el modo
+        strncat(automatic, list, sizeof list);//Concatena lo de arriba con la lista
         system(automatic);
+
+    } else if (strcmp(argv[1], "2") == 0) { //El dos significa Modo 2 y es automatico
+        strncat (interactive, &argv[2], sizeof argv[2]);//Concatena el comando con el modo
+        system(interactive);
         
     } else {
         printf ("ERROR\n");
