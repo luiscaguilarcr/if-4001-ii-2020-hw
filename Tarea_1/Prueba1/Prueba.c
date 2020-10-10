b086cb3b0059fcbfd0b10ef80032e7c563bad17c
Luis Carlos Aguilar Morales <luiscarlos.aguilar@ucr.ac.cr>
2020-10-10 15:54:07 -0600

    Update

--- Tarea_1/Prueba1/Prueba.c
+++ Tarea_1/Prueba1/Prueba.c
@@ -1,22 +1,30 @@
-#include <stdlib.h>
+#include <linux/kernel.h>
+#include <sys/syscall.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <string.h>
+#include <errno.h>
 
-void main(int argc, char *argv[]) {
-    char automatic[] = "strace -c ls > "; //Modo automatico 
-    char interactive[] = "strace -c ls "; //Modo interactivo
-    char list[] = "/null";
-    
-    if(strcmp(argv[1], "1") == 0){ //El uno significa Modo 1 y es automatico
+int main(int argc, char *argv[])
+{
+    char automatic[] = "strace -c ls > "; //Modo automatico
+    char interactive[] = "strace ls 2>&1 > ";//Modo interactivo
+    char list[] = "/Lista de directorios";
+
+    if (strcmp(argv[1], "1") == 0)                    //El uno significa Modo 1 y es automatico
+    {                                                 
         strncat(automatic, &argv[2], sizeof argv[2]); //Concatena el comando con el modo
-        strncat(automatic, list, sizeof list);//Concatena lo de arriba con la lista
+        strncat(automatic, list, sizeof list);        //Concatena lo de arriba con la lista
         system(automatic);
-
-    } else if (strcmp(argv[1], "2") == 0) { //El dos significa Modo 2 y es automatico
-        strncat (interactive, &argv[2], sizeof argv[2]);//Concatena el comando con el modo
-        system(interactive);
-        
-    } else {
-        printf ("ERROR\n");
     }
+    else if (strcmp(argv[1], "2") == 0)                 //El dos significa Modo 2 y es interactivo
+    {                                                   
+        strncat(interactive, &argv[2], sizeof argv[2]); //Concatena el comando con el modo
+        system(interactive);
+    }
+    else
+    {
+        printf("ERROR\n");
+    }
+    return 0;
 }
